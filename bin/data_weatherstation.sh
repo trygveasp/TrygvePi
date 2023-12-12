5b97f25fd095566196ab676a5a80c0ec9d866647
Trygve Aspelien <trygveasp@met.no>
2023-12-12 12:39:39 +0100

    Use real path

--- bin/data_weatherstation.sh
+++ bin/data_weatherstation.sh
@@ -4,8 +4,9 @@ set -x
 location=`cat $HOME/.weatherstation_location`
 station=`cat $HOME/.weatherstation_name`
 
-cred="$HOME/.netatmo.credentials.copy"
+cred="$HOME/.netatmo.credentials"
 gpg --import $location/data/pub/trygveasp-gpg.pub || exit 1
+[ -f $cred ] && cp $cred ${cred}.copy
 gpg -d -o $cred $location/data/$station.secret.gpg || exit 1
 
 sleep 3
