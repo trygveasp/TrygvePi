7cf5e6c9821b712fc595060c5a0f26a1438e68d8
Trygve Aspelien <trygveasp@met.no>
2025-01-07 09:51:56 +0100

    Correct crontab syntax

--- bin/start_weatherstation.sh
+++ bin/start_weatherstation.sh
@@ -5,5 +5,5 @@ station=`cat $HOME/.weatherstation_name`
 
 unset CLIENT_ID CLIENT_SECRET REFRESH_TOKEN
 set -x
-crontab -f $location/data/crontab
+crontab $location/data/crontab
 python $location/weatherStation/weatherStation.py $station
